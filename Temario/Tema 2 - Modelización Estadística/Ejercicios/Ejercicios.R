0f9ffa76709d07a02661a6104e4b1aba0e21da18
Galán <antoniogalanG15@gmail.com>
2021-04-05 03:49:29 +0200

    Acentos eliminados para prevenir erratas
    Se volvió a cambiar el formato a UTF-8.

--- Temario/Tema 2 - Modelización Estadística/Ejercicios/Ejercicios.R
+++ Temario/Tema 2 - Modelización Estadística/Ejercicios/Ejercicios.R
@@ -4,13 +4,13 @@ library(tidyverse)
 
 # EJERCICIO 03 =================================================================
 
-# Datos del enunciado (tabla)
+# Datos del enunciado: tabla
 X  <- c(1,2,3,4,5)
 Y1 <- c(4,2,3,2,4)
 Y2 <- c(1,3,5,7,9)
 
 
-# Coeficientes de correlación
+# Coeficientes de correlacion
 cor(X,Y1)
 cor(X,Y2)
 
@@ -18,7 +18,7 @@ cor(X,Y2)
 
 # EJERCICIO 04 =================================================================
 
-# Datos del enunciado (tabla)
+# Datos del enunciado: tabla
 densidad  <- c(43,55,40,52,39,33,50,33,44,21)
 velocidad <- c(27.0,23.8,30.7,24.0,34.8,41.4,27.0,40.4,31.7,51.2)
 
@@ -40,7 +40,7 @@ cor(densidad,velocidad)
 
 # EJERCICIO 08 =================================================================
 
-# Datos del enunciado (tabla)
+# Datos del enunciado: tabla
 X <- c(1,2,3,4,5)
 Y <- c(3.0,4.5,7.0,10.0,15.0)
 
@@ -48,14 +48,14 @@ Y <- c(3.0,4.5,7.0,10.0,15.0)
 # Ajuste
 ajuste <- nls(y ~ a*b^x)
 
-# Representación
+# Representacion
 plot(X,Y);abline(ajuste)
 
 
 
 # EJERCICIO 09 =================================================================
 
-# Datos del enunciado (tabla)
+# Datos del enunciado: tabla
 X <- c(1,2,3,4,5)
 Y <- c(0.5,2.0,4.5,8.0,12.5)
 
@@ -63,14 +63,14 @@ Y <- c(0.5,2.0,4.5,8.0,12.5)
 # Ajuste
 ajuste <- nls(y ~ a*x^b)
 
-# Representación
+# Representacion
 plot(X,Y);abline(ajuste)
 
 
 
 # EJERCICIO 10 =================================================================
 
-# Datos del enunciado (tabla)
+# Datos del enunciado: tabla
 X <- c(1,2,3,4,5)
 Y <- c(1.00,0.50,0.33,0.25,0.20)
 
@@ -78,14 +78,14 @@ Y <- c(1.00,0.50,0.33,0.25,0.20)
 # Ajuste
 ajuste <- nls(y ~ 1/(a+b^x))
 
-# Representación
+# Representacion
 plot(X,Y);abline(ajuste)
 
 
 
 # EJERCICIO 11 =================================================================
 
-# Datos del enunciado (puntos)
+# Datos del enunciado: puntos
 x <- c(1,2,3,4,5)
 y <- c(1,1,2,4,8)
 
@@ -126,7 +126,7 @@ prueba_2 <- exp(coeficientes_2[1]) * exp(coeficientes_2[2])^p
 
 # EJERCICIO 12 =================================================================
 
-# Datos del enunciado (puntos)
+# Datos del enunciado: puntos
 X <- c(20,30,40,50)
 Y <- c(0,1,2)
 
@@ -134,7 +134,7 @@ tabla <- matrix(c(2,0,0,1,3,2,1,3,2,2,0,0), nrow=4, byrow=T)
 dimnames(tabla) <- list(X,Y)
 
 
-# Apartados (a) ----------------------------------------------------------------
+# Apartado (a) -----------------------------------------------------------------
 ajuste <- lm(y ~x)
 
 plot(tabla)
