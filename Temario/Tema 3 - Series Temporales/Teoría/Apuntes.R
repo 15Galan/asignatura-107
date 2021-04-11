92004308c2d676b381bd3595a0f6f6d1beb1caf8
Galán <antoniogalanG15@gmail.com>
2021-04-11 22:00:58 +0200

    Apuntes de R del Tema 3 modificados
    Se añadió más contenido y el fichero de ejemplo con los datos.

--- Temario/Tema 3 - Series Temporales/Teoría/Apuntes.R
+++ Temario/Tema 3 - Series Temporales/Teoría/Apuntes.R
@@ -6,7 +6,8 @@
 
 
 # Datos que se usaran en estos ejemplos
-datos <- scan("C:/ts01.dat")
+ruta  <- paste(getwd(), "/Recursos/ts01.dat", sep="")  # Ruta relativa
+datos <- scan(ruta)
 
 
 # Convertir los datos en un objeto de serie temporal
@@ -28,70 +29,133 @@ lines(tenden, col=2)  # Añade la tendencia al grafico anterior
 
 
 # HIPOTESIS MULTIPLICATIVA =====================================================
-
-  # Obtener la Comp. Estacionaria (E)
-  esta <- serie / tenden  # Normalizar la tendencia
-  plot(esta)
   
+  # DESCOMPOSICION (MANUAL) ----------------------------------------------------
   
-  # Eliminar la Comp. Aleatoria
-  data   <- matrix(esta, byrow=T, ncol=4) # Componente estacionaria como matriz
-  medias <- colMeans(data, na.rm=T)       # Calcula las medias por columna (sin NAs)
-  
-    # La media de las columnas deberia ser 1, pero
-    # no es el caso, por lo que debe normalizarse
-    medias
-    mean(medias)
+    # Obtener la Comp. Estacionaria (E)
+    esta <- serie / tenden  # Normalizar la tendencia
+    plot(esta)
     
     
-    # Normalizar la media de las columnas
-    norm <- medias / mean(medias)
+    # Eliminar la Comp. Aleatoria
+    data   <- matrix(esta, byrow=T, ncol=4) # Componente estacionaria como matriz
+    medias <- colMeans(data, na.rm=T)       # Calcula las medias por columna (sin NAs)
+    
+      # La media de las columnas deberia ser 1, pero
+      # no es el caso, por lo que debe normalizarse
+      medias
+      mean(medias)
+      
+      
+      # Normalizar la media de las columnas
+      norm <- medias / mean(medias)
+      
+    
+    # Obtener la Comp. Aleatoria (A)
+    alea <- esta / norm
     
   
-  # Obtener la Comp. Aleatoria (A)
-  alea <- esta / norm
+
+  # DESCOMPOSICION (AUTOMATICA) ------------------------------------------------
+    
+    # Usando 'decompose()'
+    desc <- decompose(serie, type="mul")
+    plot(desc)
+    
+    
+    
+  # DESESTACIONALIZACION -------------------------------------------------------
   
-
-
+    # Serie desestacionalizada
+    deses <- serie / norm
+    plot(deses)
+    
+    
+    # Prediccion lineal
+    xx     <- 1:80            # Porque la serie tiene 80 valores
+    modelo <- lm(deses ~ xx)
+    ajuste <- predict.lm(modelo)
+    predic <- ts(ajuste, start=c(1969,1), frequency=4)
+    
+    lines(predic, col=3)  # Añadir la prediccion al grafico anterior
+    
+    
+    
 # HIPOTESIS ADITIVA ============================================================
   
-  # Obtener la Comp. Estacionaria (E)
-  esta <- serie - tenden  # Normalizar la tendencia
-  plot(esta)
-  
-  
-  # Eliminar la Comp. Aleatoria
-  data   <- matrix(esta, byrow=T, ncol=4) # Componente estacionaria como matriz
-  medias <- colMeans(data, na.rm=T)       # Calcula las medias por columna (sin NAs)
-  
-    # La media de las columnas deberia ser 0, pero
-    # no es el caso, por lo que debe normalizarse
-    medias
-    mean(medias)
+  # DESCOMPOSICION (MANUAL) ----------------------------------------------------
+    
+    # Obtener la Comp. Estacionaria (E)
+    esta <- serie - tenden  # Normalizar la tendencia
+    plot(esta)
     
     
-    # Normalizar la media de las columnas
-    norm <- medias - mean(medias)
+    # Eliminar la Comp. Aleatoria
+    data   <- matrix(esta, byrow=T, ncol=4) # Componente estacionaria como matriz
+    medias <- colMeans(data, na.rm=T)       # Calcula las medias por columna (sin NAs)
     
+      # La media de las columnas deberia ser 0, pero
+      # no es el caso, por lo que debe normalizarse
+      medias
+      mean(medias)
+      
+      
+      # Normalizar la media de las columnas
+      norm <- medias - mean(medias)
+      
+    
+    # Obtener la Comp. Aleatoria (A)
+    alea <- esta - norm
+    
+    
+    
+  # DESCOMPOSICION (AUTOMATICA) ------------------------------------------------
+    
+    # Usando 'decompose()'
+    desc <- decompose(serie, type="add")
+    plot(desc)
   
-  # Obtener la Comp. Aleatoria (A)
-  alea <- esta - norm
   
+  # DESESTACIONALIZACION -------------------------------------------------------
   
+    # Serie desestacionalizada
+    deses <- serie - norm
+    plot(deses)
+    
+    
+    # Prediccion lineal
+    xx     <- 1:80            # Porque la serie tiene 80 valores
+    modelo <- lm(deses ~ xx)
+    ajuste <- predict.lm(modelo)
+    predic <- ts(ajuste, start=c(1969,1), frequency=4)
+    
+    lines(predic, col=3)  # Añadir la prediccion al grafico anterior
+    
+    
+    
+# DESCOMPOSICION CON FALLOS ====================================================
   
-# FORMA AUTOMATICA =============================================================
-
-  # Descomposicion segun la hipotesis multiplicativa
-  desc_M <- decompose(serie, type="mul")
-  plot(desc_M)
-  
-  # Descomposicion segun la hipotesis aditiva
-  desc_A <- decompose(serie, type="add")
-  plot(desc_A)
-  
-  
-  # Descomponer incorrectamente
-  serie2 <- ts(datos, frequency=3)        # Si la Comp. Aleatoria presenta
+  # Suponiendo que se usan los mismos datos, donde ya se sabe que la frecuencia
+  # es 4, pero esta vez se usara 3 de forma erronea
+    
+  serie2 <- ts(datos, frequency=3)        # Si la Comp. Aleatoria (A) presenta
   error  <- decompose(serie2, type="add") # patrones, posiblemente la frecuencia
   plot(error)                             # sea incorrecta
   
+  
+  
+# AUTOCORRELACION ==============================================================
+
+  # Reajustar los datos
+  datos1 <- head(datos, -4) # Todos los valores excepto los 4 ultimos
+  datos2 <- tail(datos, -4) # Todos los valores excepto los 4 primeros
+  
+  # Crear un modelo de regresion lineal
+  modelo <- lm(datos1 ~ datos2)
+  summary(modelo)
+  
+    # El modelo ofrece un coef. corr. lineal de 0.9678 con una frecuencia de 4;
+    # el coef. corr. lineal varia con distintas frecuencias, y en este caso como
+    # se obtiene el mayor coef. corr. lineal, indica que 4 es el mejor valor;
+    # significa que «desplazando los valores cada 4 encajan muy bien entre ellos»
+    
