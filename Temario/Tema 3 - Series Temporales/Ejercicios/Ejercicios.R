041a5da58d80a871e63a9f7c49bbf5dea721f80c
Galán <antoniogalanG15@gmail.com>
2021-04-28 01:10:10 +0200

    Ejercicios de R del Tema 3 añadidos
    Resueltos: 1, 2, 3, 5 y 6.

--- Temario/Tema 3 - Series Temporales/Ejercicios/Ejercicios.R
+++ Temario/Tema 3 - Series Temporales/Ejercicios/Ejercicios.R
@@ -72,37 +72,89 @@ produccion <- c(3.9, 4, 4.8, 5.1, 5, 5.5, 6.1, 6.3, 6.9)
 serie <- ts(produccion, start=c(2016,1), frequency=3)
 
 
-# Obtener la tendencia
-filtro <-  c(1, 1, 1) / 3
-tenden <- filter(serie, filtro)
+  # Manualmente ---------------------------------------------------------------
+
+    # Obtener la tendencia
+    filtro <-  c(1, 1, 1) / 3
+    tenden <- filter(serie, filtro)
 
 
-# Representacion grafica
-plot(serie)
-lines(tenden, col=2)
+    # Representacion grafica
+    plot(serie)
+    lines(tenden, col=2)
 
 
-# Obtener la Comp. Estacionaria (E)
-esta <- serie / tenden
+    # Obtener la Comp. Estacionaria (E)
+    esta <- serie / tenden
 
 
-# Eliminar la Comp. Aleatoria
-data   <- matrix(esta, ncol=3, byrow=T)
-medias <- colMeans(data, na.rm=T)
+    # Eliminar la Comp. Aleatoria
+    data   <- matrix(esta, ncol=3, byrow=T)
+    medias <- colMeans(data, na.rm=T)
 
-  # La media de las columnas debería ser 1, pero
-  # como no es el caso, hay que normalizarla
-  mean(medias)
+      # La media de las columnas debería ser 1, pero
+      # como no es el caso, hay que normalizarla
+      mean(medias)
 
-  # Normalizar la media de las columnas
-  norm <- medias / mean(medias)
+      # Normalizar la media de las columnas
+      indices <- medias / mean(medias)
 
 
-# Nueva serie temporal
-esta2 <- ts(rep(norm, 3), frequency=3)
+    # Nueva serie temporal
+    esta2 <- ts(rep(indices, 3), frequency=3)
 
-# Serie desestacionalizada
-deses <- serie / esta2
+    # Serie desestacionalizada
+    deses <- serie / esta2
 
-# Obtener la Comp. Aleatoria (A)
-alea <- esta2 / norm
+    # Obtener la Comp. Aleatoria (A)
+    alea <- esta2 / norm
+
+
+  # Automaticamente -----------------------------------------------------------
+
+    # Descomponer la serie
+    desc <- decompose(serie, type="mul")
+
+
+    # Indices de la serie
+    indices <- desc$figure
+
+
+
+# EJERCICIO 05 ================================================================
+
+# Datos del enunciado: tabla
+precios <- c(25,29,34,38,42,45,70,77)
+names(precios) <- 1975:1982
+
+
+# Precios en base al precio de 1975
+precios2 <- precios / 25
+names(precios2) <- 1975:1982
+
+
+# Los resultados reflejan la variacion del precio de 1 Kg de azucar entre 1975
+# y 1982, tomando como base el precio del primer año, 25 u. Se puede observar
+# que el precio fue aumentando hasta 1985, donde el kilo valió 3.08 veces mas
+# que en 1975.
+
+  # Si se habla de %, esto serviria para decir que 1 Kg de azucar aumento su
+  # precio un 208 % en 7 años (3.08 - 1 = 2.08 -> 208 %).
+
+
+
+# EJERCICIO 06 ================================================================
+
+# Datos del enunciado
+# 1998 / 1990 = 1.35
+
+  # Apartado (a) --------------------------------------------------------------
+
+  # Aplicando la regla de 3: 1000 / vA = 1.35
+  valorA = 1000 / 1.35
+
+
+  # Apartado (b) --------------------------------------------------------------
+
+  # Aplicando la regla de 3: vB / 1000 = 1.35
+  valorB = 1.35 * 1000
