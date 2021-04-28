e1b804a356f6bcf24705a2b0897ab144e59827ff
Galán <antoniogalanG15@gmail.com>
2021-04-28 21:08:44 +0200

    Ejercicios de R del Tema 3 añadidos
    Resueltos: 1, 2, 3, 4 (casi), 5, 6 y 7.

--- Temario/Tema 3 - Series Temporales/Ejercicios/Ejercicios.R
+++ Temario/Tema 3 - Series Temporales/Ejercicios/Ejercicios.R
@@ -58,8 +58,26 @@ serie <- ts(poblacion, start=c(1973,1))
   
 
 
-# EJERCICIO 02
-  # Resuelto en Notion
+# EJERCICIO 02 ================================================================
+
+# (a) Falso.
+# La tendencia puede obtenerse a traves de otros metodos,
+# el de medias moviles solo es uno de ellos.
+
+
+# (b) Verdadero.
+# Una serie esta compuesta por las componentes 'T', 'E', 'C' y 'A',
+# siendo 'E' la componente estacionaria.
+# Desestacionalizar una serie consiste en eliminar 'E'.
+
+
+# (c) Falso.
+# Si los datos son cuatrimestrales, lo normal es usar 4 datos (uno por cuatrimestre).
+
+
+# (d) Verdadero.
+# Los datos no pueden ser claros, pero la pendiente de la tendencia es positiva,
+# lo que implica que los valores crecen.
 
 
 
@@ -121,6 +139,73 @@ serie <- ts(produccion, start=c(2016,1), frequency=3)
 
 
 
+# EJERCICIO 04 ================================================================
+
+# Datos del enunciado: tabla
+consumo <- c(178.2,156.7,164.2,153.2,157.5,172.6,185.9,185.8,165.0,163.6,169.0,183.1,
+             196.3,162.8,168.6,156.9,168.2,180.2,197.9,195.9,176.0,166.4,166.3,183.9,
+             197.3,173.7,173.2,159.7,175.2,187.4,202.6,205.6,185.6,175.6,176.3,191.7,
+             209.5,186.3,183.0,169.5,178.2,186.7,202.4,204.9,180.6,179.8,177.4,188.9,
+             200.0,188.7,187.5,168.6,175.7,189.4,216.1,215.4,191.5,178.5,178.6,195.6,
+             205.2,179.6,185.4,172.4,177.7,202.7,220.2,210.2,186.9,181.4,175.6,195.6)
+
+
+# Apartado (a) ----------------------------------------------------------------
+
+  # Crear un objeto de tipo serie temporal
+  serie <- ts(consumo, start=c(1976,1), frequency=12)
+
+
+  # Obtener la tendencia
+  filtro <- c(.5,1,1,1,1,1,1,1,1,1,1,1,.5) / 12
+  tenden <- filter(serie, filtro)
+
+
+  # Obtener la Comp. Estacionaria (E)
+  esta <- serie / tenden
+
+
+  # Obtener las medias por columnas
+  data <- matrix(esta, ncol=12, byrow=T)
+  medias <- colMeans(data, na.rm=T)
+
+    # Normalizar las medias
+    indices <- medias / mean(medias)
+
+
+# Apartado (b) ----------------------------------------------------------------
+
+  # Desestacionalizar
+  deses <- serie / indices
+
+
+# Apartado (c) ----------------------------------------------------------------
+
+  # Representacion grafica
+  plot(serie)
+  lines(deses, col=2)
+
+
+# Apartado (d) ----------------------------------------------------------------
+
+  # Prediccion lineal
+  xx     <- 1:72            # Porque la serie tiene 12 * 6 valores
+  modelo <- lm(deses ~ xx)
+  ajuste <- predict.lm(modelo)
+  predic <- ts(ajuste, start=c(1976,1), frequency=12)
+
+
+  # Representacion grafica
+  plot(tenden)              # Tendencia
+  lines(predic, col=3)      # Tendencia real
+
+
+# Apartado (e) ----------------------------------------------------------------
+
+  # Partiendo de X/E = T·C·A, siendo 'X/E'...
+
+
+
 # EJERCICIO 05 ================================================================
 
 # Datos del enunciado: tabla
@@ -146,7 +231,7 @@ names(precios2) <- 1975:1982
 # EJERCICIO 06 ================================================================
 
 # Datos del enunciado
-# 1998 / 1990 = 1.35
+# I_{1998 / 1990} = 1.35
 
   # Apartado (a) --------------------------------------------------------------
 
@@ -158,3 +243,18 @@ names(precios2) <- 1975:1982
 
   # Aplicando la regla de 3: vB / 1000 = 1.35
   valorB = 1.35 * 1000
+
+
+
+# EJERCICIO 07 ================================================================
+
+# Datos del enunciado
+# I_{1990 / 1969} = 10.00
+# I_{1990 / 2006} =  0.75
+
+
+# Se sabe que x / 24000 = 0.75, y que x / y = 10.00,
+# siendo 'x' el precio en 1990 e 'y' el precio en 1969.
+
+x <- 0.75 * 24000
+y <- x / 10.00
